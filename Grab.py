8d849c2b4d2aaf6b5af38c5f7122a6383b3b5f55
DaCryptoRaccoon <129953346+DaCryptoRaccoon@users.noreply.github.com>
2023-11-08 03:18:39 +0000

    Update Grab.py
    In this version, the resolution has been set to 1920x1080 (HD resolution) and the bitrate to 5000k, which should help maintain the high quality of the video during the download process. Additionally, the CRF has been set to 18 to further improve the visual quality

--- Grab.py
+++ Grab.py
@@ -20,9 +20,9 @@ if response.status_code == 200:
         # Create a new HLS stream URL for the variant playlist
         variant_url = '/'.join(url.split('/')[:-1]) + '/' + variant_url
 
-        # Adjust the FFmpeg encoding settings for higher quality
+        # Adjust the FFmpeg encoding settings for higher quality and resolution
         input_stream = ffmpeg.input(variant_url)
-        output_stream = ffmpeg.output(input_stream, 'output.mp4', **{'c:v': 'libx264', 'crf': 23, 'preset': 'slow', 'movflags': 'faststart'})
+        output_stream = ffmpeg.output(input_stream, 'output.mp4', s='1920x1080', b='5000k', **{'c:v': 'libx264', 'crf': 18, 'preset': 'slow', 'movflags': 'faststart'})
         ffmpeg.run(output_stream, overwrite_output=True)
 
         print("Video downloaded successfully as 'output.mp4' with improved encoding settings.")
